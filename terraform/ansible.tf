d8bd25e6c8c48a6bc644c106ac9878613a9be329
james-otten <jamesotten1@gmail.com>
2024-12-07 11:39:44 -0500

    fix

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -2,7 +2,7 @@ resource "ansible_group" "ntp_mgt_group" {
   name = "ntp_mgt"
   variables = {
     ansible_user                 = var.mesh_local_user
-    ansible_ssh_private_key_file = "../terraform/messh"
+    ansible_ssh_private_key_file = "../terraform/ntp"
     ansible_ssh_common_args      = "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
     DATADOG_API_KEY              = var.datadog_api_key
     DATADOG_SITE                 = var.datadog_site
